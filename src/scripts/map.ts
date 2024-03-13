3f1fc79b19ee703f60bd69de9f5b3f8fd1ef28f6
James Grubb <james@jamesgrubb.co.uk>
2024-03-13 15:06:31 +0000

    Download SVG

--- src/scripts/map.ts
+++ src/scripts/map.ts
@@ -8,8 +8,35 @@ const map = new mapboxgl.Map({
     style: 'mapbox://styles/makingthings/cltotxevf001a01pngsz9ayiw',
     center:[0 , 0],
     zoom: 0.6,
-    interactive: false,
-})
+    interactive: false,        
+},
+)
+
+
+// var map = new mapboxgl.Map({
+//     container: 'map',
+//     style: {
+//         version: 8,
+//         sources: {
+//             'mapbox-dem': {
+//                 type: 'raster-dem',
+//                 url: 'mapbox://styles/makingthings/cltotxevf001a01pngsz9ayiw',
+//                 tileSize: 512,
+//                 maxzoom: 14
+//             }
+//         },
+//         layers: [
+//           {
+//             id: 'background',
+//             type: 'background',
+//             paint: { 
+//               'background-color': 'red' 
+//             }
+//           }
+//         ]
+
+//       },
+// });
 
 
 
